f15a96a3f53a2f2bf00d1882241c2360c2849aa7
Kai Hendry <hendry@iki.fi>
2015-12-03 09:56:46 +0800

    This setcookie code is needed to reset the session_id

--- clockout.php
+++ clockout.php
@@ -25,6 +25,14 @@ if (file_exists($p)) {
 // Unset all of the session variables.
 $_SESSION = array();
 
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
 if (session_destroy()) {
 	$url = 'http://' . $_SERVER['HTTP_HOST'];
 	header('Location: ' . $url);
